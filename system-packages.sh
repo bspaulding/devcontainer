cfb16d5f99584cf4e186bd473f862ec1cea57c17
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-04-01 22:47:19 -0700

    extracted packages to var

--- system-packages.sh
+++ system-packages.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
+PACKAGES="git tmux vim fish bash"
 echo -n "Installing system packages for $1"
 case $1 in
-	alpine) apk add --no-cache git tmux vim fish bash;;
-	ubuntu) apt-get update && apt-get install -y git tmux vim fish bash;;
+	alpine) apk add --no-cache $PACKAGES;;
+	ubuntu) apt-get update && apt-get install -y $PACKAGES;;
 	*) echo -n "unimplemented base tag '$1'" && exit 1;;
 esac
