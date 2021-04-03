d954886fc7de69f76d1fa4d2106181034451f18d
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-04-03 14:50:30 -0700

    system package install looks for a package manager

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ARG base
 WORKDIR /root
 
 COPY system-packages.sh system-packages.sh
-RUN sh system-packages.sh $base
+RUN sh system-packages.sh
 RUN rm system-packages.sh
 
 RUN git clone https://github.com/bspaulding/dotfiles.git $HOME/dotfiles
--- system-packages.sh
+++ system-packages.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
 
 PACKAGES="git tmux vim fish bash"
-echo -n "Installing system packages for $1"
-case $1 in
-	alpine) apk add --no-cache $PACKAGES;;
-	ubuntu) apt-get update && apt-get install -y $PACKAGES;;
-	*) echo -n "unimplemented base tag '$1'" && exit 1;;
-esac
+echo -n "Installing system packages..."
+if [ -x $(which apk) ]; then
+	apk add --no-cache $PACKAGES
+elif [ -x $(which apt-get) ]; then
+	apt-get update && apt-get install -y $PACKAGES
+else
+	echo -n "error: couldn't find an implemented package manager!" && exit 1
+fi
