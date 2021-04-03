af9ce6164707e5b39ec2ea42786fe65a70513e3f
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-04-03 15:15:56 -0700

    fixed system packages script

--- system-packages.sh
+++ system-packages.sh
@@ -2,9 +2,9 @@
 
 PACKAGES="git tmux vim fish bash"
 echo -n "Installing system packages..."
-if [ -x $(which apk) ]; then
+if [ -n "$(command -v apk)" ]; then
 	apk add --no-cache $PACKAGES
-elif [ -x $(which apt-get) ]; then
+elif [ -n "$(command -v apt-get)" ]; then
 	apt-get update && apt-get install -y $PACKAGES
 else
 	echo -n "error: couldn't find an implemented package manager!" && exit 1
