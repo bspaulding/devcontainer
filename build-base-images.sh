2d632899ebb87f1934b40c43b66b034fcfdb9693
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-04-06 23:52:17 -0700

    building and pushing base images

--- build-base-images.sh
+++ build-base-images.sh
@@ -2,5 +2,6 @@
 
 for f in ./base-images/*; do
 	echo "building $(basename $f)"
-	docker build $f -t bspaulding-dev-base-$(basename $f)
+	docker build $f -t bspaulding-dev-base-$(basename $f):latest
+	docker push bspaulding-dev-base-$(basename $f):latest
 done
